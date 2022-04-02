7e5a3861a202c7e50e4edec751cef707e83b6469
Kyryl Kogut <Kyryl.Kogut@diasemi.com>
2022-04-02 10:44:40 +0300

    Set minimum supported version of macOs

--- premake5.lua
+++ premake5.lua
@@ -19,8 +19,8 @@ project "GLFW"
 	}
 
     filter "system:macosx"
-        systemversion "latest"
-		pic "On"
+        systemversion "12.1"
+        pic "On"
 
         files
         {
@@ -45,57 +45,6 @@ project "GLFW"
         {
             "_GLFW_COCOA"
         }
-
-	filter "system:linux"
-		pic "On"
-
-		systemversion "latest"
-		
-		files
-		{
-			"src/x11_init.c",
-			"src/x11_monitor.c",
-			"src/x11_window.c",
-			"src/xkb_unicode.c",
-			"src/posix_time.c",
-			"src/posix_thread.c",
-			"src/glx_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c",
-			"src/linux_joystick.c"
-		}
-
-		defines
-		{
-			"_GLFW_X11"
-		}
-
-	filter "system:windows"
-		systemversion "latest"
-
-		files
-		{
-			"src/win32_init.c",
-			"src/win32_joystick.c",
-			"src/win32_monitor.c",
-			"src/win32_time.c",
-			"src/win32_thread.c",
-			"src/win32_window.c",
-			"src/wgl_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c"
-		}
-
-		defines 
-		{ 
-			"_GLFW_WIN32",
-			"_CRT_SECURE_NO_WARNINGS"
-		}
-
-		links
-		{
-			"Dwmapi.lib"
-		}
 
 	filter "configurations:Debug"
 		runtime "Debug"
